5deb030a869fd1727c987f77b5d7f92e6dd358ec
Glenn Davis Wahome <wahomeglenn@gmail.com>
2026-04-16 11:46:19 +0300

    changed index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 
+const createLoginTracker = (user) => {
+  let wrongAttempts = 0;
+  let locked = false;
 
+  return (passwordGuess) => {
+    if (locked) {
+      return 'Account locked due to too many failed login attempts';
+    }
+
+    if (passwordGuess === user.password) {
+      return 'Login successful';
+    }
+
+    wrongAttempts += 1;
+    if (wrongAttempts > 3) {
+      locked = true;
+      return 'Account locked due to too many failed login attempts';
+    }
+
+    return `Attempt ${wrongAttempts}: Login failed`;
+  };
+};
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
